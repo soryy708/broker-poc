d7f315e3a9d01783ae318f7bae9695284664fd52
Climax708 <soryy708@gmail.com>
2022-03-10 18:18:32 +0200

    Fix misnomer

--- src/entityGateway/payment/find.ts
+++ src/entityGateway/payment/find.ts
@@ -3,8 +3,8 @@ import { IPayment } from '../../types/payment';
 
 async function find(paymentId: string, dependencies: Dependencies = null): Promise<IPayment> {
     dependencies = injectDependencies(dependencies, ['db']);
-    const createdPayment = await dependencies.db.models.payment.findByPk(paymentId);
-    return createdPayment;
+    const payment = await dependencies.db.models.payment.findByPk(paymentId);
+    return payment;
 }
 
 export default find;
